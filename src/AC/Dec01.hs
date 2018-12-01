3f4b71cbc04bec14aef7cc572cc30f629fa7db81
Mike Pickering <michael.pickering@tstllc.net>
2018-12-01 15:48:24 -0500

    Clean up repeated calculation of new frequency

--- src/AC/Dec01.hs
+++ src/AC/Dec01.hs
@@ -20,6 +20,7 @@ part2 changes = findRepeatedFrequency (cycle changes) 0 Set.empty
 -- TODO do we really need Num a, Ord a?  Is there a better way to express this?
 findRepeatedFrequency :: (Num a, Ord a) => [a] -> a -> Set.Set a -> a
 findRepeatedFrequency (change:changes) freq seen
-    | Set.member (freq + change) seen = freq + change
-    | otherwise = findRepeatedFrequency changes (freq + change) (Set.insert freq seen)
+    | Set.member newfreq seen = newfreq
+    | otherwise = findRepeatedFrequency changes newfreq (Set.insert freq seen)
+    where newfreq = freq + change
 findRepeatedFrequency _ _ _ = error "list must contain at least two elements"
